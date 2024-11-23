92fa734bc6061f8a3f9ad4d9145a8b5e2e5864de
Nuutti Kotivuori <naked@iki.fi>
2024-11-23 15:48:55 +0000

    Ignore instead of test for panic

--- src/day15.rs
+++ src/day15.rs
@@ -1,12 +1,12 @@
 use test_case::test_case;
 
-#[test_case("inputs/input-15.txt" => panics)]
+#[test_case("inputs/input-15.txt" => ignore)]
 pub fn puzzle1(filename: &str) -> i64 {
     todo!("todo: {}", filename);
 }
 
 
-#[test_case("inputs/input-15.txt" => panics)]
+#[test_case("inputs/input-15.txt" => ignore)]
 pub fn puzzle2(filename: &str) -> i64 {
     todo!("todo: {}", filename);
 }
