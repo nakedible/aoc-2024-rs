4d527abcea51b69ec779348afaf6b20d44c1a384
Nuutti Kotivuori <naked@iki.fi>
2024-11-23 15:45:15 +0000

    Add placeholders for all days

---
+++ src/day07.rs
@@ -0,0 +1,12 @@
+use test_case::test_case;
+
+#[test_case("inputs/input-07.txt" => panics)]
+pub fn puzzle1(filename: &str) -> i64 {
+    todo!("todo: {}", filename);
+}
+
+
+#[test_case("inputs/input-07.txt" => panics)]
+pub fn puzzle2(filename: &str) -> i64 {
+    todo!("todo: {}", filename);
+}
