604446168d3d2e493534707230d6aee29488f35f
Nuutti Kotivuori <naked@iki.fi>
2024-12-01 05:24:17 +0000

    Optimize day 1

--- src/day01.rs
+++ src/day01.rs
@@ -5,34 +5,34 @@ pub fn parse_input(filename: &str) -> (Vec<i64>, Vec<i64>) {
     let mut a = Vec::new();
     let mut b = Vec::new();
     for line in data.lines() {
-        let parts: Vec<i64> = line.split("   ").map(|x| x.parse().unwrap()).collect();
-        a.push(parts[0]);
-        b.push(parts[1]);
+        let (av, bv) = line.split_once("   ").unwrap();
+        a.push(av.parse().unwrap());
+        b.push(bv.parse().unwrap());
     }
     (a, b)
 }
 
 #[test_case("inputs/input-01.txt" => 1197984)]
-#[test_case("inputs/example-01-1.txt" => 0)]
+#[test_case("inputs/example-01-1.txt" => 11)]
 pub fn puzzle1(filename: &str) -> i64 {
     let (mut a, mut b) = parse_input(filename);
 
     a.sort();
     b.sort();
     let ret = a.iter().zip(b.iter()).map(|(x, y)| (x - y).abs()).sum();
-
     ret
 }
 
 
-#[test_case("inputs/input-01.txt" => 0)]
+#[test_case("inputs/input-01.txt" => 23387399)]
+#[test_case("inputs/example-01-1.txt" => 31)]
 pub fn puzzle2(filename: &str) -> i64 {
     let (a, b) = parse_input(filename);
 
-    let mut sum = 0;
-    for v in a.iter() {
-        sum += v * (b.iter().filter(|&x| x == v).count()) as i64;
-    }
- 
-    sum
+    let counts = b.iter().fold(std::collections::HashMap::new(), |mut acc, x| {
+        *acc.entry(*x).or_insert(0i64) += 1;
+        acc
+    });
+    let ret = a.iter().map(|x| x * counts.get(x).unwrap_or(&0)).sum();
+    ret
 }
