eb69143551edc8715ff2ecf9a403c2a1926460d8
Nuutti Kotivuori <naked@iki.fi>
2024-12-02 05:26:22 +0000

    Day 2 done

--- src/day02.rs
+++ src/day02.rs
@@ -1,12 +1,56 @@
 use test_case::test_case;
 
-#[test_case("inputs/input-02.txt" => ignore)]
-pub fn puzzle1(filename: &str) -> i64 {
-    todo!("todo: {}", filename);
+fn parse_input(filename: &str) -> Vec<Vec<i64>> {
+    let data = std::fs::read_to_string(filename).unwrap();
+    let mut ret = Vec::new();
+    for line in data.lines() {
+        ret.push(
+            line.split(' ')
+                .map(|x| x.parse().unwrap())
+                .collect::<Vec<i64>>(),
+        );
+    }
+    ret
 }
 
+#[test_case("inputs/input-02.txt" => 534)]
+pub fn puzzle1(filename: &str) -> i64 {
+    let reports = parse_input(filename);
+    let mut ret = 0;
+    for report in reports {
+        let diffs: Vec<i64> = report.windows(2).map(|x| x[1] - x[0]).collect();
+        if diffs.iter().all(|x| *x >= 1 && *x <= 3) {
+            ret += 1;
+        }
+        if diffs.iter().all(|x| *x >= -3 && *x <= -1) {
+            ret += 1;
+        }
+    }
+    ret
+}
 
-#[test_case("inputs/input-02.txt" => ignore)]
+#[test_case("inputs/input-02.txt" => 577)]
+#[test_case("inputs/example-02-1.txt" => 4)]
 pub fn puzzle2(filename: &str) -> i64 {
-    todo!("todo: {}", filename);
+    let reports = parse_input(filename);
+    let mut ret = 0;
+    for report in reports {
+        for i in 0..report.len() {
+            let mut report = report.clone();
+            report.remove(i);
+            let diffs: Vec<i64> = report.windows(2).map(|x| x[1] - x[0]).collect();
+            let mut safe = false;
+            if diffs.iter().all(|x| *x >= 1 && *x <= 3) {
+                safe = true;
+            }
+            if diffs.iter().all(|x| *x >= -3 && *x <= -1) {
+                safe = true;
+            }
+            if safe {
+                ret += 1;
+                break;
+            }
+        }
+    }
+    ret
 }
