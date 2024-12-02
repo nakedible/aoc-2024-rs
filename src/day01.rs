5273471d3722abbb404183a26b1d782c0e69bd4c
Nuutti Kotivuori <naked@iki.fi>
2024-12-02 05:26:42 +0000

    Reformat day one

--- src/day01.rs
+++ src/day01.rs
@@ -1,6 +1,6 @@
 use test_case::test_case;
 
-pub fn parse_input(filename: &str) -> (Vec<i64>, Vec<i64>) {
+fn parse_input(filename: &str) -> (Vec<i64>, Vec<i64>) {
     let data = std::fs::read_to_string(filename).unwrap();
     let mut a = Vec::new();
     let mut b = Vec::new();
@@ -23,16 +23,17 @@ pub fn puzzle1(filename: &str) -> i64 {
     ret
 }
 
-
 #[test_case("inputs/input-01.txt" => 23387399)]
 #[test_case("inputs/example-01-1.txt" => 31)]
 pub fn puzzle2(filename: &str) -> i64 {
     let (a, b) = parse_input(filename);
 
-    let counts = b.iter().fold(std::collections::HashMap::new(), |mut acc, x| {
-        *acc.entry(*x).or_insert(0i64) += 1;
-        acc
-    });
+    let counts = b
+        .iter()
+        .fold(std::collections::HashMap::new(), |mut acc, x| {
+            *acc.entry(*x).or_insert(0i64) += 1;
+            acc
+        });
     let ret = a.iter().map(|x| x * counts.get(x).unwrap_or(&0)).sum();
     ret
 }
