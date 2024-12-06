7bdd15ebfd160dacf4e2337bf85c698e5da5169c
Nuutti Kotivuori <naked@iki.fi>
2024-12-06 21:24:47 +0000

    Change day 6 to use bytes

--- src/day06.rs
+++ src/day06.rs
@@ -1,20 +1,20 @@
 use pathfinding::matrix::{directions, Matrix};
 use test_case::test_case;
 
-fn parse_input(filename: &str) -> Matrix<char> {
+fn parse_input(filename: &str) -> Matrix<u8> {
     let input = std::fs::read_to_string(filename).unwrap();
-    Matrix::from_rows(input.lines().map(|l| l.chars())).unwrap()
+    Matrix::from_rows(input.lines().map(|l| l.bytes())).unwrap()
 }
 
-fn dir_char(dir: (isize, isize)) -> char {
+fn dir_char(dir: (isize, isize)) -> u8 {
     if dir == directions::N {
-        '^'
+        b'^'
     } else if dir == directions::E {
-        '>'
+        b'>'
     } else if dir == directions::S {
-        'v'
+        b'v'
     } else if dir == directions::W {
-        '<'
+        b'<'
     } else {
         unreachable!()
     }
@@ -34,12 +34,12 @@ fn turn_right(dir: (isize, isize)) -> (isize, isize) {
     }
 }
 
-fn do_step(map: &mut Matrix<char>, pos: &mut (usize, usize), dir: &mut (isize, isize)) -> bool {
+fn do_step(map: &mut Matrix<u8>, pos: &mut (usize, usize), dir: &mut (isize, isize)) -> bool {
     let Some(next_pos) = map.move_in_direction(*pos, *dir) else {
         map[*pos] = dir_char(*dir);
         return false;
     };
-    if map[next_pos] == '#' {
+    if map[next_pos] == b'#' {
         *dir = turn_right(*dir);
     } else {
         map[*pos] = dir_char(*dir);
@@ -52,12 +52,12 @@ fn do_step(map: &mut Matrix<char>, pos: &mut (usize, usize), dir: &mut (isize, i
 #[test_case("inputs/example-06-1.txt" => 41)]
 pub fn puzzle1(filename: &str) -> i64 {
     let mut map = parse_input(filename);
-    let mut pos = map.items().find(|(_, &c)| c == '^').unwrap().0;
+    let mut pos = map.items().find(|(_, &c)| c == b'^').unwrap().0;
     let mut dir = directions::N;
     while do_step(&mut map, &mut pos, &mut dir) {}
     let ret = map
         .values()
-        .filter(|&&c| matches!(c, '^' | '>' | 'v' | '<'))
+        .filter(|&&c| matches!(c, b'^' | b'>' | b'v' | b'<'))
         .count() as i64;
     ret
 }
@@ -67,7 +67,7 @@ pub fn puzzle1(filename: &str) -> i64 {
 pub fn puzzle2(filename: &str) -> i64 {
     let orig_map = parse_input(filename);
     let mut map = orig_map.clone();
-    let pos = map.items().find(|(_, &c)| c == '^').unwrap().0;
+    let pos = map.items().find(|(_, &c)| c == b'^').unwrap().0;
     let dir = directions::N;
     let mut path = Vec::new();
     {
@@ -84,7 +84,7 @@ pub fn puzzle2(filename: &str) -> i64 {
         map.as_mut().copy_from_slice(orig_map.as_ref());
         let mut pos = pos;
         let mut dir = dir;
-        map[obs] = '#';
+        map[obs] = b'#';
         while do_step(&mut map, &mut pos, &mut dir) {
             if map[pos] == dir_char(dir) {
                 sum += 1;
