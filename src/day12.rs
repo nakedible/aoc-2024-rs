25f2a79e590f0ed09c2932dd1ee5aa768f828fc7
Nuutti Kotivuori <naked@iki.fi>
2024-12-12 21:43:09 +0000

    Optimize day 12

--- src/day12.rs
+++ src/day12.rs
@@ -60,42 +60,42 @@ fn count_plot2(
     let mut queue = VecDeque::new();
     queue.push_back(pos);
     let mut area = 0;
-    let mut edges: HashSet<(isize, isize)> = HashSet::new();
+    let mut edges = Matrix::new(garden.rows * 3, garden.columns * 3, b' ');
     while let Some(pos) = queue.pop_front() {
         if seent.contains(&pos) {
             continue;
         }
         seent.insert(pos);
         area += 1;
-        edges.insert((pos.0 as isize * 3 - 1, pos.1 as isize * 3 + 0));
-        edges.insert((pos.0 as isize * 3 + 0, pos.1 as isize * 3 + 1));
-        edges.insert((pos.0 as isize * 3 + 1, pos.1 as isize * 3 + 0));
-        edges.insert((pos.0 as isize * 3 + 0, pos.1 as isize * 3 - 1));
+        edges[(pos.0 * 3 + 1 - 1, pos.1 * 3 + 1 + 0)] = b'#';
+        edges[(pos.0 * 3 + 1 + 1, pos.1 * 3 + 1 + 0)] = b'#';
+        edges[(pos.0 * 3 + 1 + 0, pos.1 * 3 + 1 - 1)] = b'#';
+        edges[(pos.0 * 3 + 1 + 0, pos.1 * 3 + 1 + 1)] = b'#';
         for dir in DIRECTIONS_4.iter() {
             let Some(neigh) = garden.move_in_direction(pos, *dir) else {
                 continue;
             };
             if garden[neigh] == garden[pos] {
-                edges.remove(&(pos.0 as isize * 3 + dir.0, pos.1 as isize * 3 + dir.1));
+                edges[(pos.0 * 3 + (1 + dir.0) as usize, pos.1 * 3 + (1 + dir.1) as usize)] = b' ';
                 queue.push_back(neigh);
             }
         }
     }
-    for i in -1..garden.rows as isize * 3 {
-        for j in -1..garden.columns as isize * 3 {
-            if edges.contains(&(i, j)) && edges.contains(&(i, j - 3)) {
-                edges.remove(&(i, j - 3));
+    for i in 0..garden.rows * 3 {
+        for j in 0..garden.columns * 3 {
+            if j >= 3 && edges[(i, j)] == b'#' && edges[(i, j - 3)] == b'#' {
+                edges[(i, j - 3)] = b' ';
             }
         }
     }
-    for j in -1..garden.columns as isize * 3 {
-        for i in -1..garden.rows as isize * 3 {
-            if edges.contains(&(i, j)) && edges.contains(&(i - 3, j)) {
-                edges.remove(&(i - 3, j));
+    for j in 0..garden.columns * 3 {
+        for i in 0..garden.rows * 3 {
+            if i >= 3 && edges[(i, j)] == b'#' && edges[(i - 3, j)] == b'#' {
+                edges[(i - 3, j)] = b' ';
             }
         }
     }
-    area * edges.len() as i64
+    area * edges.values().filter(|&&b| b == b'#').count() as i64
 }
 
 #[test_case("inputs/input-12.txt" => 844132)]
